0d47b95ccaa8ad9b6365dc7b42c9618439347789
mshanm <muthu@absolut-e.com>
2019-10-18 18:53:52 -0400

    dockerized.

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.7.4
+
+RUN pip install pytest
+
+RUN pip install flask
+
---
+++ run_help.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+docker build -t battleship-ai .
+
+docker run -it --rm \
+-v /Users/muthu/dev/project/battleship-ai:/battleship-ai \
+battleship-ai bin/bash
+
+cd battleship-ai/src && python game.py
+
+
