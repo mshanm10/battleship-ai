52aa14ae845f626185cfe46324e033b662286bf2
mshanm <muthu@absolut-e.com>
2019-10-19 14:24:20 -0400

    random player added.

--- src/game.py
+++ src/game.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import helper as hl
 import datetime
+import itertools
+import random
+import time
 
 tg_grid = pd.DataFrame(np.zeros((5, 5), dtype=int), columns=['A', 'B', 'C', 'D', 'E'], index=[1, 2, 3, 4, 5])
 B = 0  # blank
@@ -13,6 +16,30 @@ oc_grid = pd.DataFrame([[0, 0, 0, 0, S],
                         [S, S, S, 0, S],
                         [0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0]], columns=['A', 'B', 'C', 'D', 'E'], index=[1, 2, 3, 4, 5])
+
+
+def real_user():
+    return input(f"{player['name']}'s attack (eg:A1)):").upper()
+
+
+class RandomPlayer(object):
+
+    def __init__(self, player):
+        self.moves_available = list(itertools.product(tg_grid.columns, tg_grid.index))
+        random.shuffle(self.moves_available)
+
+    def input(self):
+        print(f"{player['name']}'s attack (eg:A1)): random player playing...(please wait)")
+        time.sleep(2)
+        return ''.join(map(str, self.moves_available.pop()))
+
+
+# def random_player():
+#     if moves_available == None:
+#         moves_available =
+
+
+
 player_a = {
     "name": "Player A",
     "ocean_grid": oc_grid.copy(),
@@ -33,7 +60,7 @@ MISS = 'Miss'
 def play(player, opponent, f_obj):
     print(f"--------{player['name']}'s Targeting Grid---------")
     print(f"{player['targeting_grid']}")
-    move = input(f"{player['name']}'s attack (eg:A1)):").upper()
+    move = player['get_input']()
     col = move[0]
     ind = int(move[1:])
     val = opponent['ocean_grid'][col][ind]
@@ -59,6 +86,7 @@ def play(player, opponent, f_obj):
         print(f"> > > > > > > > > > > > > {status}")
         return True
 
+
 def write_data(targeting_grid, status, f_obj):
     f_obj.write(f"{','.join(targeting_grid.to_numpy().flatten().astype(str))}, {status}")
 
@@ -67,11 +95,13 @@ if __name__ == '__main__':
     print("Battleship Game Started.")
     c_time = datetime.datetime.now().ctime()
     player = player_a
+    player['get_input'] = real_user
     opponent = player_b
+    opponent['get_input'] = RandomPlayer(opponent).input
     player_filename = f"{player['name']}-moves-status-{c_time}.txt"
     opponent_filename = f"{opponent['name']}-moves-status-{c_time}.txt"
     with open(player_filename, 'w') as player_f, \
-        open(opponent_filename, 'w') as opponent_f:
+            open(opponent_filename, 'w') as opponent_f:
         while play(player, opponent, player_f):
             player, opponent = opponent, player
             player_f, opponent_f = opponent_f, player_f
