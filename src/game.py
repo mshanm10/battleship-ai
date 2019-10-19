624689494160950b232d278a1222a44921048420
mshanm <muthu@absolut-e.com>
2019-10-19 12:59:44 -0400

    game engine working.

--- src/game.py
+++ src/game.py
@@ -13,39 +13,50 @@ oc_grid = pd.DataFrame([[0, 0, 0, 0, S],
                         [0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0]], columns=['A', 'B', 'C', 'D', 'E'], index=[1, 2, 3, 4, 5])
 player_a = {
+    "name": "Player A",
     "ocean_grid": oc_grid.copy(),
     "targeting_grid": tg_grid.copy()
 }
 player_b = {
+    "name": "Player B",
     "ocean_grid": oc_grid.copy(),
     "targeting_grid": tg_grid.copy()
 }
 
 
-if __name__ == '__main__':
-    print("Battleship Game Started.")
-    print("Your Targeting Grid (Player A)")
-    print("-----BEFORE------")
-    print(player_a['targeting_grid'])
-    print(player_b['ocean_grid'])
-    move = input("Player A (your move):").upper()
+def play(player, opponent):
+    print(f"--------{player['name']}'s Targeting Grid---------")
+    print(f"{player['targeting_grid']}")
+    # print(other_player['ocean_grid'])
+    move = input(f"{player['name']}'s attack (eg:A1)):").upper()
     col = move[0]
     ind = int(move[1:])
-    print(move)
-    val = player_b['ocean_grid'][col][ind]
+    # print(move)
+    val = opponent['ocean_grid'][col][ind]
     if val == S:
-        player_a['targeting_grid'][col][ind] = H
-        player_b['ocean_grid'][col][ind] = H
-        if hl.ship_sunk(player_b['ocean_grid'], col, ind):
-            if hl.all_ships_sunk(player_b['ocean_grid']):
-                print('Won')
+        player['targeting_grid'][col][ind] = H
+        opponent['ocean_grid'][col][ind] = H
+        if hl.ship_sunk(opponent['ocean_grid'], col, ind):
+            if hl.all_ships_sunk(opponent['ocean_grid']):
+                print(f"{player['name']} Won  !!!!!!!!!!!!!!!!!!!")
+                return False
             else:
-                print('Ship Sunk')
+                print('\/ \/ \/ \/ Ship Sunk \/ \/ \/ \/')
         else:
-            print('Hit')
+            print('* * * * * * * Hit * * * * * * * * ')
     else:
-        print('Miss')
-        player_a['targeting_grid'][col][ind] = M
-    print("-----AFTER------")
-    print(player_a['targeting_grid'])
-    print(player_b['ocean_grid'])
+        print('- - - - -  - Miss - - - - - - - - ')
+        player['targeting_grid'][col][ind] = M
+    return True
+
+
+if __name__ == '__main__':
+    print("Battleship Game Started.")
+    game_over = False
+    player = player_a
+    opponent = player_b
+    while play(player, opponent):
+        player, opponent = opponent, player
+    print(f"Thanks for defending PicnicHealth")
+
+
